fa18fb6e960e10e5101f63b22f7ce78660cf6949
unknown <PhilmacFLy@.(none)>
2009-10-14 16:22:54 +0200

    Intazial Sync, only Gui
    Signed-off-by:  PhilmacFLy

---
+++ godeterm.dpr
@@ -0,0 +1,14 @@
+program godeterm;
+
+uses
+  Forms,
+  godeterm_main in 'godeterm_main.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
---
+++ godeterm_main.pas
@@ -0,0 +1,44 @@
+unit godeterm_main;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, ExtCtrls;
+
+type
+  TForm1 = class(TForm)
+    a11: TLabeledEdit;
+    a12: TLabeledEdit;
+    a13: TLabeledEdit;
+    a21: TLabeledEdit;
+    a22: TLabeledEdit;
+    a23: TLabeledEdit;
+    a31: TLabeledEdit;
+    a32: TLabeledEdit;
+    a33: TLabeledEdit;
+    b1: TLabeledEdit;
+    b2: TLabeledEdit;
+    b3: TLabeledEdit;
+    btt_her: TButton;
+    D: TLabeledEdit;
+    D1: TLabeledEdit;
+    D2: TLabeledEdit;
+    D3: TLabeledEdit;
+    X1: TLabeledEdit;
+    X2: TLabeledEdit;
+    X3: TLabeledEdit;
+  private
+    { Private-Deklarationen }
+  public
+    { Public-Deklarationen }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+end.
